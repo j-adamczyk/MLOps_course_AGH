e0f3e59e1894250848613b91260a4155973bd608
Jakub Adamczyk <jakubadamczyk10@gmail.com>
2025-10-01 16:11:36 +0200

    Add lab 2

---
+++ lab02/data/services_table.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS services;
+CREATE TABLE services (
+    "Service:RDT-ID" INT,
+    "Service:Date" DATE,
+    "Service:Type" TEXT,
+    "Service:Company" TEXT,
+    "Service:Train number" INT,
+    "Service:Completely cancelled" BOOLEAN,
+    "Service:Partly cancelled" BOOLEAN,
+    "Service:Maximum delay" INT,
+    "Stop:RDT-ID" INT,
+    "Stop:Station code" TEXT,
+    "Stop:Station name" TEXT,
+    "Stop:Arrival time" TIMESTAMP,
+    "Stop:Arrival delay" INT,
+    "Stop:Arrival cancelled" BOOLEAN,
+    "Stop:Departure time" TIMESTAMP,
+    "Stop:Departure delay" INT,
+    "Stop:Departure cancelled" BOOLEAN
+);
+CREATE INDEX service_rdt_id ON services("Service:RDT-ID");
+CREATE INDEX service_date ON services("Service:Date");
